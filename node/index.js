3ee6e35b23b1ec599e7e907b3c9433e16ef18165
Steve Minor <stv.mnr@gmail.com>
2025-09-12 22:29:14 -0700

    added python example, updated js a bit

--- node/index.js
+++ node/index.js
@@ -2,54 +2,52 @@ const fs = require("fs");
 const readlineSync = require("readline-sync");
 const TEMPLATE_DIR = "../templates";
 const tagRegex = /{([^}]+)}/g;
-let fileName = process.argv.find(arg => arg.indexOf(".madlib") !== -1);
+let fileName = process.argv.find((arg) => arg.indexOf(".madlib") !== -1);
 
 const printHeadline = () => {
-    console.log("*".repeat(25));
-    console.log(" ".repeat(9) + "MADLIBS" + " ".repeat(9));
-    console.log("*".repeat(25));
+  console.log("*".repeat(25));
+  console.log(" ".repeat(9) + "MADLIBS" + " ".repeat(9));
+  console.log("*".repeat(25));
 };
 
 printHeadline();
 
 if (!fileName) {
-    console.log("\nAvailable Templates:");
-    const files = fs.readdirSync(TEMPLATE_DIR);
-    const index = readlineSync.keyInSelect(files, "Which Template");
-    fileName = files[index];
+  console.log("\nAvailable Templates:");
+  const files = fs.readdirSync(TEMPLATE_DIR);
+  const index = readlineSync.keyInSelect(files, "Which Template");
+  fileName = files[index];
 }
 
 let template;
 try {
-    template = fs.readFileSync(`${TEMPLATE_DIR}/${fileName}`);
+  template = fs.readFileSync(`${TEMPLATE_DIR}/${fileName}`);
 } catch (e) {
-    throw "There was a problem opening your file. Please check the filename and permissions";
+  throw "There was a problem opening your file. Please check the filename and permissions";
 }
 
 const matchDict = {};
 let match = tagRegex.exec(template);
 
-while (match){
-    const token = match[1];
-    const replacementToken = readlineSync.question(`Enter a ${token.toLowerCase()}: `);
+while (match) {
+  const token = match[1];
+  const replacementToken = readlineSync.question(
+    `Enter a ${token.toLowerCase()}: `
+  );
 
-    matchDict[token] = matchDict[token] || {replacements: [], replaceIndex: 0};
-    matchDict[token].replacements.push(replacementToken);
-    match = tagRegex.exec(template);
+  matchDict[token] ??= [];
+  matchDict[token].push(replacementToken);
+  match = tagRegex.exec(template);
 }
 
-const splitTemplate = template.toString().split(tagRegex).map(token => {
-    if (token in matchDict){
-        const {
-            replacements,
-            replaceIndex: index
-        } = matchDict[token];
-
-        const newToken = replacements[index];
-        matchDict[token].replaceIndex++;
-        return newToken;
+const splitTemplate = template
+  .toString()
+  .split(tagRegex)
+  .map((token) => {
+    if (token in matchDict) {
+      return matchDict[token].pop();
     }
     return token;
-});
+  });
 
 console.log(`\nYour madlib:\n\n${splitTemplate.join("")}`);
--- node/index2.js
+++ node/index2.js
@@ -2,53 +2,57 @@ const fs = require("fs");
 const readlineSync = require("readline-sync");
 const TEMPLATE_DIR = "../templates";
 const madLib = [];
-const nextOpeningTagIndex = (templateString, from = 0) => templateString.indexOf("{", from);
-const nextClosingTagIndex = (templateString, from = 0) => templateString.indexOf("}", from);
-const isVowel = char => /[aeiou]/i.test(char);
+const nextOpeningTagIndex = (templateString, from = 0) =>
+  templateString.indexOf("{", from);
+const nextClosingTagIndex = (templateString, from = 0) =>
+  templateString.indexOf("}", from);
+const isVowel = (char) => /[aeiou]/i.test(char);
 let stringIndex = 0;
-let fileName = process.argv.find(arg => arg.indexOf(".madlib") !== -1);
+let fileName = process.argv.find((arg) => arg.indexOf(".madlib") !== -1);
 
 const printHeadline = () => {
-    console.log("*".repeat(25));
-    console.log(" ".repeat(9) + "MADLIBS" + " ".repeat(9));
-    console.log("*".repeat(25));
+  console.log("*".repeat(25));
+  console.log(" ".repeat(9) + "MADLIBS" + " ".repeat(9));
+  console.log("*".repeat(25));
 };
 
 printHeadline();
 
 if (!fileName) {
-    console.log("\nAvailable Templates:");
-    const files = fs.readdirSync(TEMPLATE_DIR);
-    const index = readlineSync.keyInSelect(files, "Which Template");
-    fileName = files[index];
+  console.log("\nAvailable Templates:");
+  const files = fs.readdirSync(TEMPLATE_DIR);
+  const index = readlineSync.keyInSelect(files, "Which Template");
+  fileName = files[index];
 }
 
-if (fileName === undefined){
-    process.exit(0);
+if (fileName === undefined) {
+  process.exit(1);
 }
 
 let template;
 try {
-    template = fs.readFileSync(`${TEMPLATE_DIR}/${fileName}`, "utf-8");
+  template = fs.readFileSync(`${TEMPLATE_DIR}/${fileName}`, "utf-8");
 } catch (e) {
-    throw "There was a problem opening your file. Please check the filename and permissions";
+  throw "There was a problem opening your file. Please check the filename and permissions";
 }
 
-while (stringIndex !== template.length){
-    const openingTagIndex = nextOpeningTagIndex(template, stringIndex);
-    let closingTagIndex;
+while (stringIndex !== template.length) {
+  const openingTagIndex = nextOpeningTagIndex(template, stringIndex);
+  let closingTagIndex;
 
-    if (openingTagIndex > -1) {
-        closingTagIndex = nextClosingTagIndex(template, openingTagIndex);
-        const token = template.substring(openingTagIndex + 1, closingTagIndex);
-        const replacementToken = readlineSync.question(`Enter a${isVowel(token[0]) ? "n" : ""} ${token.toLowerCase()}: `);
-        // add everything from the last closing tag up, but not including, the next opening tag
-        madLib.push(template.substring(stringIndex, openingTagIndex));
-        madLib.push(replacementToken);
-        stringIndex = closingTagIndex + 1;
-    } else {
-        stringIndex = template.length;
-    }
+  if (openingTagIndex > -1) {
+    closingTagIndex = nextClosingTagIndex(template, openingTagIndex);
+    const token = template.substring(openingTagIndex + 1, closingTagIndex);
+    const replacementToken = readlineSync.question(
+      `Enter a${isVowel(token[0]) ? "n" : ""} ${token.toLowerCase()}: `
+    );
+    // add everything from the last closing tag up to, but not including, the next opening tag
+    madLib.push(template.substring(stringIndex, openingTagIndex));
+    madLib.push(replacementToken);
+    stringIndex = closingTagIndex + 1;
+  } else {
+    stringIndex = template.length;
+  }
 }
 
 console.log(`\nYour madlib:\n\n${madLib.join("")}`);
